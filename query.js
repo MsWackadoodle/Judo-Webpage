1a567c295a23217ef8ce922c09d00d3fd11db56f
MsWackadoodle <oomanfrei@web.de>
2015-11-17 10:26:42 +0400

    sum
    added 480px media query

--- query.js
+++ query.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
-	// gets fixed top navigation
+	console.log($(window).width());
 	$(window).scroll(function() {
 		if ( $(window).scrollTop() > 700 ) {
 			$(".navbar").addClass("fixxed") 
 		} else { $(".navbar").removeClass("fixxed") }
 	});
-	// goes to particular section when clicked 
 	$("#thome").click(function(e) {
 		e.preventDefault();
 		$("#btn-btnav").addClass('collapsed');
@@ -62,7 +61,7 @@ $(document).ready(function() {
         	scrollTop: $(".contactus").offset().top -40
     	}, 2000);
 	});
-	// 'Contact Us' fields
+
 	$('#cu-name').on('blur', function() {
 		if (this.value == '') {this.value = 'Name'}
 	});
@@ -73,39 +72,12 @@ $(document).ready(function() {
 		if (this.value == '') {this.value = 'Telephone'}
 	});
 
-	// section 'Our instructors'
 	(function() {
 		if ($(window).width() <= 500 ) {
 			$('.imgstaff').each(function() {
-				$(this).prependTo($(this).parent().find('.col-xs-8'));
-				$(this).parent().css('width', '100%');
-				$(this).css({'width': '50%', 'margin': '0 25%'});
-				$(this).parent().find('h3, p').css({'text-align': 'center', 'padding': '0'});
+				$(this).appendTo($(this).parent().find('.col-xs-8'))
 			});
 		}
 	})();
-	$(window).resize(function() {
-		if ($(window).width() <= 500 ) {
-			$('.imgstaff').each(function() {
-				$(this).prependTo($(this).parent().find('.col-xs-8'));
-				$(this).parent().css('width', '100%');
-				$(this).css({'width': '50%', 'margin': '0 25%'});
-				$(this).parent().find('h3, p').css({'text-align': 'center', 'padding': '0'});
-			});
-		} else {
-			$('.imgstaff').each(function() {
-				if ($(this).parent().hasClass('left-staff')) {
-					$(this).appendTo($(this).parent().parent());
-				} else if ($(this).parent().hasClass('right-staff')) {
-					$(this).prependTo($(this).parent().parent());
-					$(this).parent().find('.col-xs-8 > h3, p').css('padding-left', '30px');
-				};
-			});
-			$('.imgstaff').css({'width':'33.33333%', 'margin': '0'});
-			$('.left-staff, .right-staff').css('width', '66.66667%');
-			$('.left-staff, .right-staff').find('h3, p').css('text-align', '');
-
-		}
-	});
 
 });
